b9b9e366572a4de37a2721042266d61c50f20bf8
Lizzie <e.tilden@wustl.edu>
2023-02-27 09:20:19 -0600

    second test

--- test/graphs.jl
+++ test/graphs.jl
@@ -1,2 +1,6 @@
 using Test
 @test connected_neighbors(graph, node) == vcat(node, graph[node])
+@test connected_neighbors(graph, node) is in reachable_nodes(graph, node)
+@test for i in connected_neighbors(graph, node)
+    connected_neighbors(graph, connected_neighbors(graph, node)[i]) is in reachable_nodes(graph, node)
+end
