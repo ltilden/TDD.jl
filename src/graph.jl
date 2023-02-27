dd267fd1672cfebdebf84f56a36827ed884da9f4
Lizzie <e.tilden@wustl.edu>
2023-02-27 10:05:41 -0600

    second implementation

--- src/graph.jl
+++ src/graph.jl
@@ -2,3 +2,10 @@ function connected_neighbors(graph, node)
     direct_connect =  vcat(node, graph[node])
     return direct_connect
 end
+
+function reachable_nodes(graph, node)
+    direct_connect = connected_neighbors(graph, node)
+    indirect_connect = [connected_neighbors(graph, i) for i in direct_connect]
+    reachable = unique(collect(Iterators.flatten(indirect_connect)))
+    return reachable
+end
